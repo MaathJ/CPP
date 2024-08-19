ebe80ea67d4cac553d101e4eda805ec019db9c97
MathhJ <matiarevalo611@gmail.com>
2024-08-19 13:49:32 -0500

    FC

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve(){
+
+    cout << "Hello World!" << endl;
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
